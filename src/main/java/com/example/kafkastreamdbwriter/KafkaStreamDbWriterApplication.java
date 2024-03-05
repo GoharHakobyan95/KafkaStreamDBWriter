998cfc9c8319d3772bee083210fce59e5832de13
Gohar <hak.goh95@gmail.com>
2024-03-05 22:10:43 +0400

    feat: Add Kafka message processing service
    - Created a new service to receive JSON messages from Kafka
    - Implemented functionality to parse JSON messages and write them to the database
    - Integrated the service into the Docker Compose setup
    
    This service listens to messages from Kafka, processes them as JSON, and stores them in the database. It enhances the functionality of the application by enabling real-time data ingestion from Kafka.

--- src/main/java/com/example/kafkastreamdbwriter/KafkaStreamDbWriterApplication.java
+++ src/main/java/com/example/kafkastreamdbwriter/KafkaStreamDbWriterApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KafkaStreamDbWriterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KafkaStreamDbWriterApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaStreamDbWriterApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/kafkastreamdbwriter/KafkaStreamDbWriterApplicationTests.java
+++ src/test/java/com/example/kafkastreamdbwriter/KafkaStreamDbWriterApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class KafkaStreamDbWriterApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
